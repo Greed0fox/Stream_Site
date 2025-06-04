e8703863bba27cd1a0f516df81d958c2ef2ac709
MrTrivix <162663636+MrXerobrinYt@users.noreply.github.com>
2025-06-04 20:20:32 +1200

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -11,31 +11,46 @@ particlesJS("particles-js",
     {
     particles: {
       number: {
-        value: 210,
+        value: 350,
         density: { enable: true, value_area: 236.74802907265777 }
       },
-      color: { value: "#ff0068" },
+      color: {
+        value: [
+          "#a22039", "#a22039", "#a22039", "#a22039", "#a22039",
+          "#67144f", "#67144f", "#67144f",
+          "#3b2975", "#3b2975",
+          "#ca9c6d" 
+        ]
+      },
       shape: {
         type: "circle",
-        stroke: { width: 0, color: "#000000" },
-        polygon: { nb_sides: 5 },
+        stroke: {
+          width: 0,
+          color: "#000000" 
+        }
       },
       opacity: {
-        value: 1,
+        value: 0.8,
         random: true,
-        anim: { enable: true, speed: 1, opacity_min: 0, sync: false }
+        anim: { 
+          enable: true,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false
+        }
       },
       size: {
-        value: 3,
+        value: 2.5,
         random: true,
-        anim: { enable: false, speed: 4, size_min: 0.3, sync: false }
+        anim: {
+          enable: true,
+          speed: 2,
+          size_min: 0.5,
+          sync: false
+        }
       },
       line_linked: {
         enable: false,
-        distance: 150,
-        color: "#ff0068",
-        opacity: 0.4,
-        width: 1
       },
       move: {
         enable: true,
@@ -45,22 +60,18 @@ particlesJS("particles-js",
         straight: false,
         out_mode: "out",
         bounce: false,
-        attract: { enable: false, rotateX: 600, rotateY: 600 }
       }
     },
     interactivity: {
       detect_on: "canvas",
       events: {
-        onhover: { enable: false, mode: "bubble" },
-        onclick: { enable: false, mode: "repulse" },
+        onhover: {
+            enable: false,
+        },
+        onclick: {
+            enable: false,
+        },
         resize: true
-      },
-      modes: {
-        grab: { distance: 400, line_linked: { opacity: 1 } },
-        bubble: { distance: 250, size: 0, duration: 2, opacity: 0, speed: 3 },
-        repulse: { distance: 400, duration: 0.4 },
-        push: { particles_nb: 4 },
-        remove: { particles_nb: 2 }
       }
     },
     retina_detect: true
